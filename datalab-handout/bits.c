19ebdf404beb8bd4de46c2b9eedb822460e21885
kali <leonchung0703@gmail.com>
2021-06-30 23:16:01 +0800

    Finish until allOddBits

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -143,7 +143,7 @@ NOTES:
  *   Rating: 1
  */
 int bitXor(int x, int y) {
-  return 2;
+  return  ~(~x & ~y) &~ (x & y);
 }
 /* 
  * tmin - return minimum two's complement integer 
@@ -152,9 +152,7 @@ int bitXor(int x, int y) {
  *   Rating: 1
  */
 int tmin(void) {
-
-  return 2;
-
+  return 0x1<<31;
 }
 //2
 /*
@@ -165,7 +163,8 @@ int tmin(void) {
  *   Rating: 1
  */
 int isTmax(int x) {
-  return 2;
+  // return !(~(x+1)^x ) &! !(x+1);
+  return !(((x+1)^x )+ 1) &!!(x+1);
 }
 /* 
  * allOddBits - return 1 if all odd-numbered bits in word set to 1
@@ -176,7 +175,11 @@ int isTmax(int x) {
  *   Rating: 2
  */
 int allOddBits(int x) {
-  return 2;
+  int a = 0xAA << 8; // 0xAA00
+  int b = 0xAA | a; // 0xAAAA
+  int c = b << 16 | b; ///0xAAAAAAAA
+  return  !(x & c ^ c);
+
 }
 /* 
  * negate - return -x 
